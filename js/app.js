11b8ab45e21c50da63ac39259fc9f0f98123cdbf
nathanth <nathan.hardenburg@icloud.com>
2022-06-05 16:45:34 -0500

    initializes game and calls render function

--- js/app.js
+++ js/app.js
@@ -1,18 +1,56 @@
+// 1) Define the required variables used to track the state of the game
+
+// 2) Store cached element references
+
+// 3) Upon loading, the game state should be initialized, and a function should be 
+//    called to render this game state
+
+// 4) The state of the game should be rendered to the user
+
+// 5) Define the required constants
+
+// 6) Handle a player clicking a square with a `handleClick` function
+
+// 7) Build the `getWinner` function
+
+// 8) Create Reset functionality
+
 /*-------------------------------- Constants --------------------------------*/
 
-
+const winningCombos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,7,4],
+  [2,5,8],
+  [2,4,6],
+  [0,4,8]
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let board = [null, null, null, null, null, null, null, null, null]
+let turn = 1
+let winner = null
 
 /*------------------------ Cached Element References ------------------------*/
 
-
+const squareEls = document.querySelectorAll('.square')
+const messageEl = document.querySelector('#message')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-
+squareEls.forEach(function(square){
+  square.addEventListener('click', handleClick)
+})
 
 /*-------------------------------- Functions --------------------------------*/
+
+function init () {
+  board = [null, null, null, null, null, null, null, null, null]
+  turn = 1
+  winner = null
+  render ()
+}
 
